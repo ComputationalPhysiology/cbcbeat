c29dbeb083655f0b9253298e4d351117a1c0046b
Henrik Finsberg <henriknf@simula.no>
2022-03-25 10:22:37 +0100

    Make sure we use the same schemes when comparing the solvers and add require goss makers to tests that use goss

--- test/unit/test_gossplittingsolver.py
+++ test/unit/test_gossplittingsolver.py
@@ -5,21 +5,22 @@ Unit tests for GOSS splitting solver
 __author__ = "Cécile Daversin Catty (cecile@simula.no), 2022"
 __all__ = ["TestGOSSplittingSolver"]
 
-from testutils import assert_almost_equal, medium, parametrize
-
-from dolfin import info
+import pytest
+from pathlib import Path
+from testutils import assert_almost_equal, medium, parametrize, require_goss
 
 from cbcbeat import *
 from cbcbeat.gossplittingsolver import GOSSplittingSolver
 
-import goss
-from goss.dolfinutils import DOLFINParameterizedODE
 
 try:
     set_log_level(LogLevel.WARNING)
 except:
     set_log_level(WARNING)
     pass
+
+
+here = Path(__file__).parent.absolute()
 
 
 class TestSplittingSolver(object):
@@ -35,8 +36,7 @@ class TestSplittingSolver(object):
         self.stimulus = Expression("2.0*t", t=self.time, degree=1)
 
         # Create ac
-        self.applied_current = Expression("sin(2*pi*x[0])*t", t=self.time,
-                                          degree=3)
+        self.applied_current = Expression("sin(2*pi*x[0])*t", t=self.time, degree=3)
 
         # Create conductivity "tensors"
         self.M_i = 1.0
@@ -44,31 +44,43 @@ class TestSplittingSolver(object):
 
         # Splitting Solver cell and cardiac models
         self.cell_model = FitzHughNagumoManual()
-        self.cardiac_model = CardiacModel(self.mesh, self.time,
-                                          self.M_i, self.M_e,
-                                          self.cell_model,
-                                          self.stimulus,
-                                          self.applied_current)
+        self.cardiac_model = CardiacModel(
+            self.mesh,
+            self.time,
+            self.M_i,
+            self.M_e,
+            self.cell_model,
+            self.stimulus,
+            self.applied_current,
+        )
+        import goss
 
         # GOSS Splitting Solver cell and cardiac models (expect different object)
         self.goss_cell_model = goss.dolfinutils.DOLFINParameterizedODE(
-            "../../cbcbeat/cellmodels/fitzhughnagumo",
-            field_states=["v", "s"])
+            here.joinpath("../../cbcbeat/cellmodels/fitzhughnagumo"),
+            field_states=["v", "s"],
+        )
 
-        self.goss_cardiac_model = CardiacModel(self.mesh, self.time,
-                                               self.M_i, self.M_e,
-                                               self.goss_cell_model,
-                                               self.stimulus,
-                                               self.applied_current)
+        self.goss_cardiac_model = CardiacModel(
+            self.mesh,
+            self.time,
+            self.M_i,
+            self.M_e,
+            self.goss_cell_model,
+            self.stimulus,
+            self.applied_current,
+        )
 
         dt = 0.1
         self.t0 = 0.0
-        self.dt = [(0.0, dt), (dt*2, dt/2), (dt*4, dt)]
+        self.dt = [(0.0, dt), (dt * 2, dt / 2), (dt * 4, dt)]
         # Test using variable dt interval but using the same dt.
 
-        self.T = self.t0 + 5*dt
+        self.T = self.t0 + 5 * dt
         self.ics = self.cell_model.initial_conditions()
 
+    @require_goss
+    @pytest.mark.goss
     @medium
     @parametrize(("solver_type"), ["direct", "iterative"])
     def test_basic_and_optimised_goss_splitting_solver_exact(self, solver_type):
@@ -77,10 +89,12 @@ class TestSplittingSolver(object):
 
         # Create GOSS splitting solver
         params = SplittingSolver.default_parameters()
+        params["enable_adjoint"] = False
         params["BasicCardiacODESolver"]["S_polynomial_family"] = "CG"
         params["BasicCardiacODESolver"]["S_polynomial_degree"] = 1
+        params["CardiacODESolver"]["scheme"] = "RL1"
         solver = SplittingSolver(self.cardiac_model, params=params)
-        
+
         (vs_, vs, vur) = solver.solution_fields()
         vs_.assign(self.ics)
 
@@ -88,12 +102,12 @@ class TestSplittingSolver(object):
         solutions = solver.solve((self.t0, self.T), self.dt)
         for (interval, fields) in solutions:
             (vs_, vs, vur) = fields
-        a = vs.vector().norm("l2")
-        c = vur.vector().norm("l2")
-        assert_almost_equal(interval[1], self.T, 1e-10)
 
-        if dolfin_adjoint:
-            adj_reset()
+        vs_original = vs
+        vur_original = vur
+        a = vs_original.vector().norm("l2")
+        c = vur_original.vector().norm("l2")
+        assert_almost_equal(interval[1], self.T, 1e-10)
 
         # Create optimised solver with direct solution algorithm
         params = GOSSplittingSolver.default_parameters()
@@ -101,32 +115,37 @@ class TestSplittingSolver(object):
         params["enable_adjoint"] = False
         if solver_type == "direct":
             params["BidomainSolver"]["use_avg_u_constraint"] = True
+        params["ode_solver"]["solver"] = "RL1"
         solver = GOSSplittingSolver(self.goss_cardiac_model, params=params)
 
         (vs_, vs, vur) = solver.solution_fields()
+        vs_.assign(self.goss_cell_model.initial_conditions())
 
         # Solve again
         solutions = solver.solve((self.t0, self.T), self.dt)
         for (interval, fields) in solutions:
             (vs_, vs, vur) = fields
         assert_almost_equal(interval[1], self.T, 1e-10)
-        b = vs.vector().norm("l2")
-        d = vur.vector().norm("l2")
+
+        vs_goss = vs
+        vur_goss = vur
+        b = vs_goss.vector().norm("l2")
+        d = vur_goss.vector().norm("l2")
 
         print("a, b = ", a, b)
         print("c, d = ", c, d)
         print("a - b = ", (a - b))
         print("c - d = ", (c - d))
 
-        err_ab = abs((a-b)*100/a)
-        err_cd = abs((c-d)*100/c)
+        err_ab = abs((a - b) * 100 / a)
+        err_cd = abs((c - d) * 100 / c)
         print("Error a - b = ", err_ab, "%")
         print("Error c - d = ", err_cd, "%")
 
         # Compare results, discrepancy is in difference in ODE
         # solves.
-        assert_almost_equal(a, b, tolerance=5.)
-        assert_almost_equal(c, d, tolerance=5.)
+        assert_almost_equal(a, b, tolerance=5.0)
+        assert_almost_equal(c, d, tolerance=5.0)
         # Discrepancy in % (less than 1% error)
-        assert_almost_equal(err_ab, 0, tolerance=1.)
-        assert_almost_equal(err_cd, 0, tolerance=1.)
+        assert_almost_equal(err_ab, 0, tolerance=1.0)
+        assert_almost_equal(err_cd, 0, tolerance=1.0)
