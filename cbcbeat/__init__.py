cd1349976b7a38d803cee11ff5b99367fac155ef
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-03-30 22:42:18 +0200

    Make it possible to still use * imports but show a deprecation warning

--- cbcbeat/__init__.py
+++ cbcbeat/__init__.py
@@ -9,8 +9,6 @@ To import the module, type::
 """
 import warnings as _warnings
 
-_warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 
 # Import all of dolfin with possibly dolfin-adjoint on top
 
@@ -64,36 +62,79 @@ parameters = _dolfin.parameters
 beat_parameters = _dolfin.Parameters("beat-parameters")
 beat_parameters.add("enable_adjoint", True)
 
+import ufl
+import ufl.log
+from ufl import *
+from ufl.log import *
+from dolfin import *
 
-__all__ = [
-    "FitzHughNagumoManual",
-    "NoCellModel",
-    "RogersMcCulloch",
-    "Beeler_reuter_1977",
-    "Tentusscher_2004_mcell",
-    "Tentusscher_panfilov_2006_epi_cell",
-    "Tentusscher_panfilov_2006_M_cell",
-    "Fenton_karma_1998_MLR1_altered",
-    "Fenton_karma_1998_BR_altered",
-    "CardiacCellModel",
-    "MultiCellModel",
-    "rhs_with_markerwise_field",
-    "Markerwise",
-    "handle_markerwise",
-    "BasicBidomainSolver",
-    "BidomainSolver",
-    "CardiacModel",
-    "CardiacODESolver",
-    "BasicCardiacODESolver",
-    "SingleCellSolver",
-    "BasicSingleCellSolver",
-    "BasicMonodomainSolver",
-    "MonodomainSolver",
-    "SplittingSolver",
-    "BasicSplittingSolver",
-    "backend",
-    "supported_cell_models",
-    "dolfinimport",
-    "parameters",
-    "beat_parameters",
-]
+
+try:
+    import dolfin_adjoint
+    from dolfin_adjoint import *
+except ImportError:
+    dolfin_adjoint = None
+    dolfin_adjoint_all = []
+else:
+    dolfin_adjoint_all = dolfin_adjoint.__all__
+
+
+class StarImportDeprecationWarning(DeprecationWarning):
+    def __init__(self, *args: object) -> None:
+        msg = """
+*** ===================================================== ***
+*** CBCBeat: Using * imports will be deprecated in the    ***
+*** next version of cbcbeat. Please change all imports    ***
+*** such as 'from cbcbeat import *' with 'import cbcbeat' ***
+*** or 'from cbcbeat import foo, bar'                     ***
+*** ===================================================== ***"""
+        super().__init__(msg)
+
+
+_warnings.simplefilter("always", StarImportDeprecationWarning)
+
+
+def __getattr__(name):
+    if name == "__all__":
+        _warnings.warn(StarImportDeprecationWarning(), stacklevel=2)
+        return (
+            dolfin_adjoint_all
+            + _dolfin.__all__
+            + ufl.__all__
+            + ufl.log.__all__
+            + [
+                "FitzHughNagumoManual",
+                "NoCellModel",
+                "RogersMcCulloch",
+                "Beeler_reuter_1977",
+                "Tentusscher_2004_mcell",
+                "Tentusscher_panfilov_2006_epi_cell",
+                "Tentusscher_panfilov_2006_M_cell",
+                "Fenton_karma_1998_MLR1_altered",
+                "Fenton_karma_1998_BR_altered",
+                "CardiacCellModel",
+                "MultiCellModel",
+                "rhs_with_markerwise_field",
+                "Markerwise",
+                "handle_markerwise",
+                "BasicBidomainSolver",
+                "BidomainSolver",
+                "CardiacModel",
+                "CardiacODESolver",
+                "BasicCardiacODESolver",
+                "SingleCellSolver",
+                "BasicSingleCellSolver",
+                "BasicMonodomainSolver",
+                "MonodomainSolver",
+                "SplittingSolver",
+                "BasicSplittingSolver",
+                "backend",
+                "supported_cell_models",
+                "dolfinimport",
+                "parameters",
+                "beat_parameters",
+            ]
+        )
+
+    msg = f"module {__name__!r} has no attribute {name!r}"
+    raise AttributeError(msg)
